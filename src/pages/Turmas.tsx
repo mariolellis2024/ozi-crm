850639e39525baa26e15828a17d6305146863d08
Mario Lellis <mariolellis@gmail.com>
2025-07-17 08:49:05 -0300

    Add space after 'Vagas:' text

--- src/pages/Turmas.tsx
+++ src/pages/Turmas.tsx
@@ -775,7 +775,7 @@ export function Turmas() {
                     <div className="space-y-2">
                       <div className={`flex items-center ${occupancyColorClass}`}>
                         <Users className="h-4 w-4 mr-2" />
-                        <span className="font-bold">Vagas:</span>
+                        <span className="font-bold">Vagas: </span>
                         <span> {turma.cadeiras - alunosMatriculados} em {sala?.nome}</span>
                       </div>
                       <div className="text-gray-400">
